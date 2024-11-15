0c51e14336bc003a7f4a6c1696385db7f3a328e6
devsohelrana <dev.sohelrana@gmail.com>
2024-11-15 20:03:20 +0600

    Add meal counte vue project

---
+++ assets/AboutView-BLMcxhkU.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as s,a as o}from"./index-C-CBQW0q.js";const a={},c={class:"container mx-auto px-6"},n=o("h1",{class:"text-primary"},"This is an about page",-1),r=[n];function _(i,d){return t(),s("div",c,r)}const f=e(a,[["render",_]]);export{f as default};
